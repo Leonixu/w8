0f793b40cec0f97a0be51062aba44825b25d7277
Leonixu <107299451+Leonixu@users.noreply.github.com>
2024-04-23 11:48:16 +1000

    Add javadox generation and publishing steps

--- src/main/java/com/example/addressbook/model/Contact.java
+++ src/main/java/com/example/addressbook/model/Contact.java
@@ -1,11 +1,24 @@
 package com.example.addressbook.model;
 
+/**
+ * Model class to represent the contact object for address book app
+ */
 public class Contact {
     private int id;
     private String firstName;
     private String lastName;
     private String email;
     private String phone;
+
+    /**
+     *
+     * @param firstName first name of the contact
+     * @param lastName last name of the contact
+     * @param email email of the contact
+     * @param phone phone of the contact
+     */
+
+
 
     public Contact(String firstName, String lastName, String email, String phone) {
         this.firstName = firstName;
